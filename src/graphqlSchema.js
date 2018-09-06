23a1478f8ac141afdfeba098d174e90c486a972a
Bryan Miller <bmiller@twig-world.com>
2018-09-06 16:01:08 +0100

    Adds Wings Query

--- src/graphqlSchema.js
+++ src/graphqlSchema.js
@@ -1,15 +1,20 @@
 import { makeExecutableSchema } from 'graphql-tools';
-import { merge } from 'lodash';
+import merge from 'lodash.merge';
 
 import CommonSchema from './common/schema';
 import CommonResolvers from './common/commonResolvers';
 
+import WingsSchema from './services/wings/wings.graphqls';
+import WingsResolvers from './services/wings/resolvers';
+
 const schema = makeExecutableSchema({
     typeDefs: [
         CommonSchema,
+        WingsSchema,
     ],
     resolvers: merge({},
         CommonResolvers,
+        WingsResolvers,
         { Query: { version: () => '1.0.0' } },
         { Mutation: { version: () => '1.0.0' } },
     ),
